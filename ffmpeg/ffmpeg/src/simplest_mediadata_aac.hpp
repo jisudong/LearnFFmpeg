b6981a661d54f4122d5fae11e29b49dc2688a8d9
jisudong <jisudong@show-fitness.com>
2019-09-10 16:35:47 +0800

    aac音频码流解析

---
+++ ffmpeg/ffmpeg/src/simplest_mediadata_aac.hpp
@@ -0,0 +1,20 @@
+//
+//  simplest_mediadata_aac.hpp
+//  ffmpeg
+//
+//  Created by 秀健身 on 2019/9/10.
+//  Copyright © 2019 秀健身. All rights reserved.
+//
+
+#ifndef simplest_mediadata_aac_hpp
+#define simplest_mediadata_aac_hpp
+
+
+/**
+ 分析 aac 码流
+
+ @param url aac文件路径
+ */
+int simplest_aac_parser(const char *url);
+
+#endif /* simplest_mediadata_aac_hpp */
