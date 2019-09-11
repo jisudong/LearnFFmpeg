8f86ae30d356b0eb8eec1a3b0fbd1b2e8c59bcf0
jisudong <jisudong@show-fitness.com>
2019-09-11 18:32:23 +0800

    udp/rtp

---
+++ ffmpeg/ffmpeg/src/simplest_mediadata_udp.cpp
@@ -0,0 +1,16 @@
+//
+//  simplest_mediadata_udp.cpp
+//  ffmpeg
+//
+//  Created by 秀健身 on 2019/9/11.
+//  Copyright © 2019 秀健身. All rights reserved.
+//
+
+#include "simplest_mediadata_udp.hpp"
+#include <iostream>
+
+
+int simplest_udp_parser(int port) {
+    
+    return 0;
+}
---
+++ ffmpeg/ffmpeg/src/simplest_mediadata_udp.hpp
@@ -0,0 +1,14 @@
+//
+//  simplest_mediadata_udp.hpp
+//  ffmpeg
+//
+//  Created by 秀健身 on 2019/9/11.
+//  Copyright © 2019 秀健身. All rights reserved.
+//
+
+#ifndef simplest_mediadata_udp_hpp
+#define simplest_mediadata_udp_hpp
+
+int simplest_udp_parser(int port);
+
+#endif /* simplest_mediadata_udp_hpp */
