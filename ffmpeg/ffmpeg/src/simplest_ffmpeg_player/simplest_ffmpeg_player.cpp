92a6701497609841130b5e667a31f019da0a495d
jisudong <jisudong@show-fitness.com>
2019-09-16 14:40:19 +0800

    导入编译后的ffmpeg库

---
+++ ffmpeg/ffmpeg/src/simplest_ffmpeg_player/simplest_ffmpeg_player.cpp
@@ -0,0 +1,26 @@
+//
+//  simplest_ffmpeg_player.cpp
+//  ffmpeg
+//
+//  Created by 秀健身 on 2019/9/16.
+//  Copyright © 2019 秀健身. All rights reserved.
+//
+
+#include "simplest_ffmpeg_player.hpp"
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+#include <libavutil/imgutils.h>
+#include <SDL2/SDL.h>
+}
+
+#define OUTPUT_YUV420P 0
+
+int ffmpeg_player() {
+    
+   
+    
+    return 0;
+}
---
+++ ffmpeg/ffmpeg/src/simplest_ffmpeg_player/simplest_ffmpeg_player.hpp
@@ -0,0 +1,14 @@
+//
+//  simplest_ffmpeg_player.hpp
+//  ffmpeg
+//
+//  Created by 秀健身 on 2019/9/16.
+//  Copyright © 2019 秀健身. All rights reserved.
+//
+
+#ifndef simplest_ffmpeg_player_hpp
+#define simplest_ffmpeg_player_hpp
+
+int ffmpeg_player();
+
+#endif /* simplest_ffmpeg_player_hpp */
