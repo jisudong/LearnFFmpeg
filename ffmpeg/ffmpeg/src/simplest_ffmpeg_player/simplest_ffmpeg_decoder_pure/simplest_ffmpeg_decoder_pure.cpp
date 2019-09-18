ca174f00060f63aead8e1fe731cb576e972aa08d
jisudong <jisudong@show-fitness.com>
2019-09-18 17:42:24 +0800

    create simplest decoder pure

---
+++ ffmpeg/ffmpeg/src/simplest_ffmpeg_player/simplest_ffmpeg_decoder_pure/simplest_ffmpeg_decoder_pure.cpp
@@ -0,0 +1,9 @@
+//
+//  simplest_ffmpeg_decoder_pure.cpp
+//  ffmpeg
+//
+//  Created by 秀健身 on 2019/9/18.
+//  Copyright © 2019 秀健身. All rights reserved.
+//
+
+#include "simplest_ffmpeg_decoder_pure.hpp"
---
+++ ffmpeg/ffmpeg/src/simplest_ffmpeg_player/simplest_ffmpeg_decoder_pure/simplest_ffmpeg_decoder_pure.hpp
@@ -0,0 +1,14 @@
+//
+//  simplest_ffmpeg_decoder_pure.hpp
+//  ffmpeg
+//
+//  Created by 秀健身 on 2019/9/18.
+//  Copyright © 2019 秀健身. All rights reserved.
+//
+
+#ifndef simplest_ffmpeg_decoder_pure_hpp
+#define simplest_ffmpeg_decoder_pure_hpp
+
+#include <stdio.h>
+
+#endif /* simplest_ffmpeg_decoder_pure_hpp */
