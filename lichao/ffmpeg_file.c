c112b1c0b4f9958a9e44f356644c3d15489d1a8e
jisudong <coderjee@163.com>
2019-12-06 14:07:32 +0800

    ffmpeg 文件删除、重命名

---
+++ lichao/ffmpeg_file.c
@@ -0,0 +1,19 @@
+#include <libavformat/avformat.h>
+
+int main(int argc, char* argv[])
+{
+	int ret;
+	ret = avpriv_io_move("111.txt", "222.txt");
+	if (ret < 0) {
+		av_log(NULL, AV_LOG_ERROR, "Failed to rename\n");
+	}
+	av_log(NULL, AV_LOG_INFO, "Success to rename\n");	
+
+	ret = avpriv_io_delete("./mytestfile.txt");
+	if (ret < 0) {
+		av_log(NULL, AV_LOG_ERROR, "Failed to delete file mytestfile.txt\n");
+		return -1;
+	}
+	av_log(NULL, AV_LOG_INFO, "Success to delete mytestfile.txt\n");
+	return 0;
+}
